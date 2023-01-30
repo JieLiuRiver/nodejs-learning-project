f7475d96f1a64a64948ed795cadc0384c5ca1035
Bode He <bode_he@epam.com>
2023-01-30 10:41:22 +0800

    feat: integrate code

--- src/router/controller/groupController.ts
+++ src/router/controller/groupController.ts
@@ -1,63 +1,100 @@
-import { Request } from 'express';
-import GroupService from '../../services/groupServices';
-import { GroupType } from '../../types/index';
+import { Request, Response } from 'express';
+import GroupService from '@/services/groupServices';
+import log from '@/services/logServices';
+import ReqMapper from '@/mapper/reqMapper';
+import { GroupType } from '@/types/index';
 
 const createGroup = async (req: Request<any, Omit<GroupType, 'id'> & {
     userIds?: string[]
 }>, res: any) => {
-    const result = await GroupService.createGroup(req.body);
-    res.formatResponse(
-        result?.status,
-        result?.message,
-        {
-            Groupid: result?.groupid
-        }
-    );
+    try {
+        const result = await GroupService.createGroup(req.body);
+        res.formatResponse(
+            result?.status,
+            result?.message,
+            {
+                Groupid: result?.groupid
+            }
+        );
+    } catch (error) {
+        log.error({
+            ...ReqMapper.toServiceApiFromReq(req),
+            error
+        });
+    }
 };
 
 const getGroupById = async (req: Request<any, any, {groupid: string}>, res: any) => {
-    const groupid = req.query.groupid;
-    const Group = await GroupService.findGroupById(groupid as string);
-    res.formatResponse(
-        Group ? 0 : -1,
-        Group ? null : `connot find group by id ${groupid}`,
-        Group || null
-    );
+    try {
+        const groupid = req.query.groupid;
+        const Group = await GroupService.findGroupById(groupid as string);
+        res.formatResponse(
+            Group ? 0 : -1,
+            Group ? null : `connot find group by id ${groupid}`,
+            Group || null
+        );
+    } catch (error) {
+        log.error({
+            ...ReqMapper.toServiceApiFromReq(req),
+            error
+        });
+    }
 };
 
 const getGroups = async (req: Request, res: any) => {
-    const Groups = await GroupService.getGroups();
-    res.formatResponse(
-        0,
-        null,
-        Groups || []
-    );
+    try {
+        const Groups = await GroupService.getGroups();
+        res.formatResponse(
+            0,
+            null,
+            Groups || []
+        );
+    } catch (error) {
+        log.error({
+            ...ReqMapper.toServiceApiFromReq(req),
+            error
+        });
+    }
 };
 
 const updateGroup = async (req: Request, res: any) => {
-    const result = await GroupService.updateGroup(req.body);
-    res.formatResponse(
-        result?.status,
-        result?.message,
-        null
-    );
+    try {
+        const result = await GroupService.updateGroup(req.body);
+        res.formatResponse(
+            result?.status,
+            result?.message,
+            null
+        );
+    } catch (error) {
+        log.error({
+            ...ReqMapper.toServiceApiFromReq(req),
+            error
+        });
+    }
 };
 
 const deleteGroup = async (req: Request<{Groupid: string}>, res: any) => {
-    const groupid = req.body.groupid;
-    if (!groupid) {
+    try {
+        const groupid = req.body.groupid;
+        if (!groupid) {
+            res.formatResponse(
+                0,
+                'groupid is required',
+                null
+            );
+        }
+        const result = await GroupService.removeGroup(groupid);
         res.formatResponse(
-            0,
-            'groupid is required',
+            result?.status,
+            result?.message,
             null
         );
+    } catch (error) {
+        log.error({
+            ...ReqMapper.toServiceApiFromReq(req),
+            error
+        });
     }
-    const result = await GroupService.removeGroup(groupid);
-    res.formatResponse(
-        result?.status,
-        result?.message,
-        null
-    );
 };
 
 export default {
