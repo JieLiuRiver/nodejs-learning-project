a70227b459d3a3bdc198868e9f598a49bb7c4698
Bode He <bode_he@epam.com>
2023-03-15 15:51:31 +0800

    perf: optimize logExceptionMiddleware

--- src/middlewares/logApiMiddleware.ts
+++ src/middlewares/logApiMiddleware.ts
@@ -5,8 +5,6 @@ import { Request, Response, NextFunction } from '@/types/index';
 
 
 export default function logApiMiddleware(req: Request<any>, res: Response, next: NextFunction) {
-    console.log();
     log.info(ReqMapper.toServiceApiFromReq(req));
-    console.log();
     next();
 }
--- src/middlewares/logExceptionMiddleware.ts
+++ src/middlewares/logExceptionMiddleware.ts
@@ -2,16 +2,7 @@
 import log from '@/services/logServices';
 import { Request, Response, NextFunction } from '@/types/index';
 
-export default function logApiMiddleware(req: Request<any>, res: Response, next: NextFunction) {
-    process.on('uncaughtException', (err: Error) => {
-        console.log();
-        log.error(err);
-        console.log();
-    });
-    process.on('unhandledRejection', (err) => {
-        console.log();
-        log.error(err);
-        console.log();
-    });
-    next();
+export default function logExceptionMiddleware(err: Error, req: Request<any>, res: Response, next: NextFunction) {
+    log.error(err);
+    next(err);
 }
